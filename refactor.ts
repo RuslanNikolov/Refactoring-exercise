4039ac456d97467501d1214617e5652b7ee79c78
Ruslan Nikolov <Ruslan_Nikolov@epam.com>
2019-09-10 14:32:57 +0300

    .

--- refactor.ts
+++ refactor.ts
@@ -4,6 +4,12 @@ export interface Item {
     quality: number;
 }
 
+const knownItemNames: { [key: string]: string } = {
+    BRIE: "Aged Brie",
+    BACKSTAGE_PASS: "Backstage passes to a TAFKAL80ETC concert",
+    SULFURAS: "Sulfuras, Hand of Ragnaros"
+};
+
 const pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
 
 export class GildedRose {
@@ -12,45 +18,48 @@ export class GildedRose {
     updateQuality(): Item[] {
         const updateUnknownItem = (item: Item): Item => {
             let newItem = { ...item }
-            const knownItemNames = [
-                "Aged Brie",
-                "Backstage passes to a TAFKAL80ETC concert",
-                "Sulfuras, Hand of Ragnaros"
-            ];
-            if (newItem.quality > 0 && !knownItemNames.includes[newItem.name])
+
+            if (newItem.quality > 0 && !Object.values(knownItemNames).includes[newItem.name]) {
                 newItem.quality--;
+            }
 
             return newItem;
         }
         const updateBackstagePassItem = (item: Item): Item => {
             let newItem = { ...item }
 
-            if (newItem.name === "Backstage passes to a TAFKAL80ETC concert" && newItem.quality < 50) {
-                if (newItem.sellIn >= 6 && newItem.sellIn < 11) {
-                    newItem.quality += 2;
-                }
-                if (newItem.sellIn < 6) {
-                    newItem.quality += 3;
-                }
+            if (newItem.name !== knownItemNames.BACKSTAGE_PASS || newItem.quality >= 50) {
+                return;
             }
+            if (newItem.sellIn >= 6 && newItem.sellIn < 11) {
+                newItem.quality += 2;
+            }
+            if (newItem.sellIn < 6) {
+                newItem.quality += 3;
+            }
+
             return newItem;
         };
 
         const updateFromSellin = (item: Item): Item => {
             let newItem = { ...item }
-            if (newItem.name !== "Sulfuras, Hand of Ragnaros") newItem.sellIn--;
 
-            if (newItem.sellIn < 0) {
-                if (newItem.name === "Aged Brie" && newItem.quality < 50) {
-                    newItem.quality++;
-                }
-                if (newItem.name === "Backstage passes to a TAFKAL80ETC concert") {
-                    newItem.quality = 0;
-                }
-                if (newItem.name !== "Sulfuras, Hand of Ragnaros" && newItem.quality > 0) {
-                    newItem.quality--;
-                }
+            if (newItem.name !== knownItemNames.SULFURAS) {
+                newItem.sellIn--;
             }
+            if (newItem.sellIn > 0) {
+                return;
+            }
+            if (newItem.name === knownItemNames.BRIE && newItem.quality < 50) {
+                newItem.quality++;
+            }
+            if (newItem.name === knownItemNames.BACKSTAGE_PASS) {
+                newItem.quality = 0;
+            }
+            if (newItem.name !== knownItemNames.SULFURAS && newItem.quality > 0) {
+                newItem.quality--;
+            }
+
             return newItem;
         };
 
